806b96e604eed620d936bebf13a0919363b221eb
jrfertonani <ademirjuniorfertonani@gmail.com>
2023-11-17 10:21:14 -0300

    Repositori

--- src/main/java/repositories/LivroCategory.java
+++ src/main/java/repositories/LivroCategory.java
@@ -1,2 +1,9 @@
-package repositories;public interface LivroCategory {
+package repositories;
+
+import domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroCategory extends JpaRepository<Livro, Integer> {
 }
