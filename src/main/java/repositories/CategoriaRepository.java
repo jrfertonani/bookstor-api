0eeb5476d0d5dadc341dc90ad723de618fb3b5fd
jrfertonani <ademirjuniorfertonani@gmail.com>
2023-11-17 10:54:04 -0300

    Repositori

--- src/main/java/repositories/CategoriaRepository.java
+++ src/main/java/repositories/CategoriaRepository.java
@@ -1,9 +1,2 @@
-package repositories;
-
-import domain.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+package repositories;public interface CategoriaRepository {
 }
